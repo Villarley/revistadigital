409b376e7a4b16c2f9bea9b156f6200e4fe62555
Santiago Villarreal Arley <115122095+Villarley@users.noreply.github.com>
2024-04-30 16:05:21 -0600

    Databases basic concepts aded!

--- src/articles/prweb/articles/Databases.jsx
+++ src/articles/prweb/articles/Databases.jsx
@@ -18,7 +18,7 @@ import {
   TabPanels,
 } from "@chakra-ui/react"
 import { Typewriter } from "@/components/shared/"
-import { NormalText } from "@/components/shared/Text"
+import { NormalText, BoldText } from "@/components/shared/Text"
 import { Pyramid } from "@/assets/images/"
 
 export default function Databases() {
@@ -27,27 +27,27 @@ export default function Databases() {
       <Flex flexDirection="column" gap={3}>
         <Typewriter text="Bases de datos" type="heading" typingSpeed={50} />
         <Tabs variant="enclosed" mt={4}>
-        <TabList>
-          <Tab _selected={{ color: "#fff", bg: "black" }}>Jerarquía de los datos</Tab>
-          <Tab _selected={{ color: "#fff", bg: "black" }}>Llaves</Tab>
-          <Tab _selected={{ color: "#fff", bg: "black" }}>Modelado de datos</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <div className="animate__animated animate__fadeInRightBig">
-              <Concepts/>
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <div className="animate__animated animate__fadeInRightBig">
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <div className="animate__animated animate__fadeIn">
-            </div>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+          <TabList>
+            <Tab _selected={{ color: "#fff", bg: "black" }}>Jerarquía de los datos</Tab>
+            <Tab _selected={{ color: "#fff", bg: "black" }}>Llaves</Tab>
+            <Tab _selected={{ color: "#fff", bg: "black" }}>Modelado de datos</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <div className="animate__animated animate__fadeInRightBig">
+                <Concepts />
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="animate__animated animate__fadeInRightBig">
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="animate__animated animate__fadeIn">
+              </div>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Flex>
     </>
   )
@@ -55,49 +55,63 @@ export default function Databases() {
 const Concepts = () => {
   return (
     <>
-      <NormalText>
-        Sirven para guardar datos y posteriormente interpretar información.
-      </NormalText>
-      <Flex justifyContent="center">
-        <Image height={400} width={400} alt="" src={Pyramid}></Image>
+      <Flex flexDirection="column" gap={3}>
+        <NormalText>
+          Sirven para guardar datos y posteriormente interpretar información.
+        </NormalText>
+        <Flex wrap="wrap" gap={3}>
+        <NormalText><BoldText>Bit.</BoldText> Unidad de información más pequeña</NormalText>
+        <NormalText><BoldText>Byte.</BoldText> Conjunto de bits.</NormalText>
+        <NormalText><BoldText>Campo.</BoldText> Conjunto de bytes.</NormalText>
+        <NormalText><BoldText>Registro.</BoldText> Conjunto de campos.</NormalText>
+        <NormalText><BoldText>Tabla.</BoldText> Conjunto de registros.</NormalText>
+        <NormalText><BoldText>Archivo.</BoldText> Conjunto de tablas.</NormalText>
+        <NormalText><BoldText>BD.</BoldText> Conjunto de archivos.</NormalText>
+        </Flex>
+        <Flex justifyContent="center">
+          <Image height={400} width={400} alt="" src={Pyramid}></Image>
+        </Flex>
+        <NormalText><BoldText>Filas.</BoldText>Están ubicados horizontalmente son los registros en la tabla.</NormalText>
+        <NormalText><BoldText>Columnas.</BoldText>Están ubicados verticalmente.</NormalText>
+        <TableContainer>
+          <Table variant='striped' colorScheme="black">
+            <TableCaption>Los registros están señalados en rojo.</TableCaption>
+            <Thead>
+              <Tr>
+                <Th color="#0083ff">Cédula</Th>
+                <Th>Nombre</Th>
+                <Th>Apellido</Th>
+                <Th>Fecha de nacimiento</Th>
+                <Th>Salario</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td color="#ff0000">1-1111-1112</Td>
+                <Td color="#ff0000">John</Td>
+                <Td color="#ff0000">Doe</Td>
+                <Td color="#ff0000">01/01/1990</Td>
+                <Td color="#ff0000">1000</Td>
+              </Tr>
+              <Tr>
+                <Td color="#0083ff">1-1111-1113</Td>
+                <Td>John</Td>
+                <Td>Doe</Td>
+                <Td>01/01/1990</Td>
+                <Td>1000</Td>
+              </Tr>
+              <Tr>
+                <Td color="#0083ff">1-1111-1114</Td>
+                <Td>John</Td>
+                <Td>Doe</Td>
+                <Td>01/01/1990</Td>
+                <Td>1000</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
       </Flex>
-      <TableContainer>
-        <Table variant='striped' colorScheme="black">
-          <TableCaption>Los registros están señalados en celeste.</TableCaption>
-          <Thead>
-            <Tr>
-              <Th color="#0083ff">Cédula</Th>
-              <Th>Nombre</Th>
-              <Th>Apellido</Th>
-              <Th>Fecha de nacimiento</Th>
-              <Th>Salario</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td color="#ff0000">1-1111-1112</Td>
-              <Td color="#ff0000">John</Td>
-              <Td color="#ff0000">Doe</Td>
-              <Td color="#ff0000">01/01/1990</Td>
-              <Td color="#ff0000">1000</Td>
-            </Tr>
-            <Tr>
-              <Td color="#0083ff">1-1111-1113</Td>
-              <Td>John</Td>
-              <Td>Doe</Td>
-              <Td>01/01/1990</Td>
-              <Td>1000</Td>
-            </Tr>
-            <Tr>
-              <Td color="#0083ff">1-1111-1114</Td>
-              <Td>John</Td>
-              <Td>Doe</Td>
-              <Td>01/01/1990</Td>
-              <Td>1000</Td>
-            </Tr>
-          </Tbody>
-        </Table>
-      </TableContainer>
+
     </>
   )
 }
