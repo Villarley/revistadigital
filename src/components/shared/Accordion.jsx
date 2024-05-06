621b2bb7e1b4feab936ca3a4d429437307d9bb8d
Santiago Villarreal Arley <115122095+Villarley@users.noreply.github.com>
2024-05-06 16:25:27 -0600

    adding content

--- src/components/shared/Accordion.jsx
+++ src/components/shared/Accordion.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { ChevronDown } from "lucide-react";
+import React from 'react'
+import { ChevronDown } from "lucide-react"
 
 export default function Accordion({ RootDirection, Clicked, handleSelectedTopic, Name }) {
   // Estilos en línea para la transición y rotación
   const chevronStyle = {
     transition: 'transform 0.5s ease',
     transform: Clicked ? 'rotate(180deg)' : 'rotate(0deg)', // Usa rotate(0deg) en lugar de 'none'
-  };
+  }
 
   return (
     <button onClick={() => Clicked ? handleSelectedTopic("") : handleSelectedTopic(RootDirection)} className="w-full flex justify-between items-center text-xl text-white">
@@ -15,5 +15,5 @@ export default function Accordion({ RootDirection, Clicked, handleSelectedTopic,
         <ChevronDown /> {/* Siempre usa el mismo ícono */}
       </span>
     </button>
-  );
+  )
 }
---
+++ src/components/shared/AccordionDescription.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AccordionDescription() {
+  return (
+    <div>AccordionDescription</div>
+  )
+}
